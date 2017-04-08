155e5986fbbd4cb3f23b9c66475cc15098523662
Vladimir Mezera <vladimir.mezera@physter.com>
2017-04-08 22:19:45 +0200

    Add arq. tests

--- src/test/java/cz/mezera/jboss/cluster/JmxClusterTest.java
+++ src/test/java/cz/mezera/jboss/cluster/JmxClusterTest.java
@@ -1,25 +1,36 @@
 package cz.mezera.jboss.cluster;
 
+import cz.meza.jboss.cluster.JmxCluster;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.remotingjmx.RemotingConnectorProvider;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import javax.management.MBeanServerConnection;
 import javax.management.ObjectName;
 import javax.management.remote.JMXConnector;
-import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- *
- */
+@RunWith(Arquillian.class)
 public class JmxClusterTest {
 
+    @Deployment
+    public static WebArchive createJavaTestArchive() {
+        return ShrinkWrap.create(WebArchive.class, "test-1.war").addAsWebInfResource("web.xml", "web.xml");
+    }
+
+    @RunAsClient
     @Test
     public void testConnect() throws Exception{
-        String host = "localhost";  // or some A.B.C.D
-        String url = "service:jmx:remoting-jmx://localhost:9999";
+        String url = "service:jmx:http-remoting-jmx://localhost:9990";
+
+        Thread.sleep(30000);
 
         RemotingConnectorProvider s = new RemotingConnectorProvider();
         JMXServiceURL serviceUrl = new JMXServiceURL(url);
@@ -28,7 +39,7 @@ public class JmxClusterTest {
         connector.connect();
         MBeanServerConnection mbeanConn = connector.getMBeanServerConnection();
 
-        mbeanConn.getAttribute(ObjectName.getInstance("jgroups:type=channel,cluster=\"web\""), "View");
+       // mbeanConn.getAttribute(ObjectName.getInstance("jgroups:type=channel,cluster=\"web\""), "View");
 
 
     }
