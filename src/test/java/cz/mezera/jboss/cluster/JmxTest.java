d6e75cea06343d6bee0522769481d75ec1375be3
Vladimir Mezera <vladimir.mezera@physter.com>
2017-04-23 22:07:15 +0200

    JBoss & wildfly profiles

--- src/test/java/cz/mezera/jboss/cluster/JmxTest.java
+++ src/test/java/cz/mezera/jboss/cluster/JmxTest.java
@@ -21,39 +21,59 @@ import java.util.Map;
 @RunWith(Arquillian.class)
 public class JmxTest {
 
-    private static final String URL = "service:jmx:remote+http://localhost:9990";
-    private static final String CHANNEL = "ee";
-    private static final String WILDFLY_JMX_URL = "jgroups:type=channel,cluster=\"" + CHANNEL + "\"";
-    private static final String WIDLFY_JMX_CLUSTER_ATTR = "view";
-
+    private static final String WILDFLY_URL = "service:jmx:remote+http://localhost:9990";
+    private static final String JBOSS_URL = "service:jmx:remoting-jmx://localhost:9999";
+    private static final String WILDFLY_CHANNEL = "ee";
+    private static final String WILDFLY_JMX_URL = "jgroups:type=channel,cluster=\"" + WILDFLY_CHANNEL + "\"";
+    private static final String JBOSS_CHANNEL = "web";
+    private static final String JBOSS_JMX_URL = "jgroups:type=channel,cluster=\"" + JBOSS_CHANNEL + "\"";
+    private static final String JMX_CLUSTER_ATTR = "view";
+    private static final String WIDLFLY_MANAGED = "wildfly-managed";
+    private static final String CONTAINER = System.getProperty("arquillian.launch", WIDLFLY_MANAGED);
 
     @Deployment
     public static WebArchive createJBossTestArchive() {
+        boolean wildfly = WIDLFLY_MANAGED.equals(CONTAINER);
+        String webDescriptor;
+        if (wildfly) {
+            webDescriptor = "web.xml";
+        } else {
+            webDescriptor = "web-jboss.xml";
+        }
         return ShrinkWrap.create(WebArchive.class, "test-jboss.war").addClass(JmxCluster.class)
-                .setWebXML("web.xml");
+                .setWebXML(webDescriptor);
     }
-
 
     @RunAsClient
     @Test
     public void testConnect() {
         try {
+
             RemotingConnectorProvider s = new RemotingConnectorProvider();
-            JMXServiceURL serviceUrl = new JMXServiceURL(URL);
+            JMXServiceURL serviceUrl;
+            boolean wildfly = WIDLFLY_MANAGED.equals(CONTAINER);
+            if (wildfly) {
+                serviceUrl = new JMXServiceURL(WILDFLY_URL);
+            } else {
+                serviceUrl = new JMXServiceURL(JBOSS_URL);
+            }
             Map<String, Object> env = new HashMap<>();
             JMXConnector connector = s.newJMXConnector(serviceUrl, env);
             connector.connect();
             MBeanServerConnection mbeanConn = connector.getMBeanServerConnection();
             Assert.assertTrue(mbeanConn.getMBeanCount() > 0);
             Thread.sleep(3000);
-            Object attr = mbeanConn.getAttribute(ObjectName.getInstance(WILDFLY_JMX_URL), WIDLFY_JMX_CLUSTER_ATTR);
+            Object attr;
+            if (wildfly) {
+                attr = mbeanConn.getAttribute(ObjectName.getInstance(WILDFLY_JMX_URL), JMX_CLUSTER_ATTR);
+            } else {
+                attr = mbeanConn.getAttribute(ObjectName.getInstance(JBOSS_JMX_URL), "View");
+            }
             Assert.assertNotNull(attr);
-
         } catch (Exception e) {
             e.printStackTrace();
             Assert.assertTrue(false);
         }
         Assert.assertTrue(true);
-
     }
 }
